1476549373ea0d2128bb997e698c857aa8bc9d45
nidhaahmed <nidhaahmed13@gmail.com>
2026-03-11 12:23:02 +0530

    setup backend server and postgres connection

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const pool = require("./db");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("StockFlow API Running");
+});
+
+app.get("/test-db", async (req, res) => {
+  const result = await pool.query("SELECT NOW()");
+  res.json(result.rows);
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
